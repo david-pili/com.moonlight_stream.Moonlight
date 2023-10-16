bbfc2a991253a630e2687c70609795e13e8c7c85
FrogTheFrog <warliukz@gmail.com>
2023-10-16 22:31:50 +0300

    Enable nightly builds

---
+++ README.md
@@ -0,0 +1,7 @@
+# Builds nightly Moonlight releases for Flatpak
+
+## One line install script (will uninstall the default version!)
+
+```
+curl -L https://raw.githubusercontent.com/FrogTheFrog/com.moonlight_stream.Moonlight/master/install.sh | sh
+```
---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+tmp_file="/tmp/moonlight-nightly.flatpak";
+
+[ -e file ] && rm $tmp_file;
+
+echo "Downloading nighly version"
+echo "-----"
+curl -L -o $tmp_file https://github.com/FrogTheFrog/com.moonlight_stream.Moonlight/releases/download/nightly/com.moonlight_stream.Moonlight.flatpak;
+echo "-----"
+
+echo "Uninstalling old flatpak version";
+sudo flatpak uninstall com.moonlight_stream.Moonlight -y &> /dev/null; # Flatpak output breaks terminal :/
+
+echo "Installing nightly flatpak version";
+sudo flatpak install "$tmp_file" -y &> /dev/null; # Flatpak output breaks terminal :/
+
+[ -e file ] && rm $tmp_file;
